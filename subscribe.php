fe879926c167cab145910f35cf65edd99bde35ac
Plamen Georgiev <npuZpak@gmail.com>
2016-11-16 20:47:14 +0200

    Updated email info

--- subscribe.php
+++ subscribe.php
@@ -10,7 +10,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the notification when someone subscribes
-    $emailTo = 'contact.azmind@gmail.com';
+    $emailTo = 'Georgiev@maxds.eu';
 
     $subscriber_email = addslashes(trim($_POST['email']));
 
